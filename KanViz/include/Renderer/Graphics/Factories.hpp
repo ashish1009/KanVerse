6b56114297d80a5a12361c7586bcb6c807d776d3
Ashish <aashishkan1009@gmail.com>
2025-10-29 07:52:04 +0530

    Factories APIs

--- KanViz/include/Renderer/Graphics/Factories.hpp
+++ KanViz/include/Renderer/Graphics/Factories.hpp
@@ -5,9 +5,33 @@
 //  Created by Ashish . on 29/10/25.
 //
 
-#ifndef Factories_hpp
-#define Factories_hpp
+#pragma once
 
-#include <stdio.h>
+// Forward Declarations
+class GLFWwindow;
 
-#endif /* Factories_hpp */
+namespace KanViz
+{
+  // KanViz Forward Declarations
+  class RendererContext;
+  class RendererAPI;
+  
+  /// This structure provides a method to create the renderer context instance based on the renderer API supported by the engine.
+  struct RendererContextFactory
+  {
+    /// This function creates the renderer context instance based on the renderer API supported by the Engine
+    /// - Parameter windowPtr: Pointer to the GLFW window.
+    /// - Returns: A scope that manages the lifetime of the created RendererContext instance.
+    /// - Note: Returns nullptr for release mode if the assertion macro is not defined.
+    [[nodiscard]] static Scope<RendererContext> Create(GLFWwindow* windowPtr);
+  };
+  
+  /// This structure provides a method to create the renderer API instance based on the supported APIs.
+  struct RendererAPIFactory
+  {
+    /// This function creates the Renderer API instance based on the Supported APIs
+    /// - Returns: A scope that manages the lifetime of the created RendererAPI instance.
+    /// - Note: Returns nullptr for release mode if the assertion macro is not defined.
+    [[nodiscard]] static Scope<RendererAPI> Create();
+  };
+} // namespace KanViz
--- KanViz/src/Renderer/Graphics/Factories.cpp
+++ KanViz/src/Renderer/Graphics/Factories.cpp
@@ -6,3 +6,48 @@
 //
 
 #include "Factories.hpp"
+
+#include "Renderer/Core/Renderer.hpp"
+
+#include "Platform/OpenGL/OpenGLRendererContext.hpp"
+#include "Platform/OpenGL/OpenGLRendererAPI.hpp"
+
+namespace KanViz
+{
+  /// Validates the current renderer API type.
+  /// - Note: Asserts if the renderer API is invalid and logs critical errors.
+  static void ValidateRendererAPI()
+  {
+    IK_LOG_CRITICAL(LogModule::Renderer, "Renderer API Type is not set or is set as invalid");
+    IK_LOG_CRITICAL(LogModule::Renderer, "Call Renderer::SetCurrentRendererAPI(RendererType) before any renderer initialization to set Renderer API type");
+    IK_LOG_CRITICAL(LogModule::Renderer, "'RendererType should not be RendererType::Invalid");
+    
+    IK_ASSERT(false, "Renderer API type is invalid; it must be set before initialization.");
+  }
+  
+  Scope<RendererContext> RendererContextFactory::Create(GLFWwindow* windowPtr)
+  {
+    switch(Renderer::GetCurrentRendererAPI())
+    {
+      case RendererType::OpenGL:
+        return CreateScope<OpenGLRendererContext>(windowPtr);
+      case RendererType::Invalid:
+      default:
+        ValidateRendererAPI();
+        return nullptr; // Adding for Release mode code where assert MACRO is not defined
+    }
+  }
+  
+  Scope<RendererAPI> RendererAPIFactory::Create()
+  {
+    switch (Renderer::GetCurrentRendererAPI())
+    {
+      case RendererType::OpenGL:
+        return CreateScope<OpenGLRendererAPI>();
+      case RendererType::Invalid:
+      default:
+        ValidateRendererAPI();
+        return nullptr; // Adding for Release mode code where assert MACRO is not defined
+    }
+  }
+} // namespace KanViz
