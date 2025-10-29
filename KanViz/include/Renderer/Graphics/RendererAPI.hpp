ef00360a1d763aad62a930cf9db357bcf9bd2f27
Ashish <aashishkan1009@gmail.com>
2025-10-29 07:49:02 +0530

    Creating Renderer API abstract class

--- KanViz/include/Renderer/Graphics/RendererAPI.hpp
+++ KanViz/include/Renderer/Graphics/RendererAPI.hpp
@@ -5,9 +5,22 @@
 //  Created by Ashish . on 29/10/25.
 //
 
-#ifndef RendererAPI_hpp
-#define RendererAPI_hpp
+#pragma once
 
-#include <stdio.h>
+#include "Renderer/Graphics/Factories.hpp"
 
-#endif /* RendererAPI_hpp */
+namespace IKan
+{
+  /// This class is the interface for all the renderer APIs.
+  /// It defines the core functions that any rendering backend must implement.
+  class RendererAPI
+  {
+  public:
+    /// Default virtual destructor for proper cleanup of derived classes.
+    virtual ~RendererAPI() = default;
+    
+    /// This function checks the renderer API error
+    /// - Parameter context: Error message to be printed
+    virtual void CheckError(std::string_view context) const = 0;
+  };
+} // namespace IKan
