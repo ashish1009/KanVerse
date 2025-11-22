fe6a5e41775407ad6424164be6dde30e28735c2c
Ashish <aashishkan1009@gmail.com>
2025-11-22 15:28:52 +0530

    Creating thread pool class

--- KanVest/include/Stock/StockManager.hpp
+++ KanVest/include/Stock/StockManager.hpp
@@ -7,10 +7,98 @@
 
 #pragma once
 
+#include "Stock/StockData.hpp"
+
 namespace KanVest
 {
+  /// Minimal thread-pool for controlled concurrency
+  class ThreadPool
+  {
+  public:
+    ThreadPool(size_t workerCount = std::thread::hardware_concurrency())
+    : m_done(false)
+    {
+      if (workerCount == 0) workerCount = 2;
+      for (size_t i = 0; i < workerCount; ++i)
+        m_workers.emplace_back([this]() { WorkerLoop(); });
+    }
+    
+    ~ThreadPool()
+    {
+      {
+        std::scoped_lock lock(m_mutex);
+        m_done = true;
+      }
+      m_cv.notify_all();
+      for (auto& t : m_workers)
+        if (t.joinable()) t.join();
+    }
+    
+    template<typename Fn>
+    void Enqueue(Fn&& fn)
+    {
+      {
+        std::scoped_lock lock(m_mutex);
+        m_queue.emplace(std::function<void()>(std::forward<Fn>(fn)));
+      }
+      m_cv.notify_one();
+    }
+    
+  private:
+    void WorkerLoop()
+    {
+      while (true)
+      {
+        std::function<void()> task;
+        {
+          std::unique_lock lock(m_mutex);
+          m_cv.wait(lock, [this]() { return m_done || !m_queue.empty(); });
+          if (m_done && m_queue.empty()) return;
+          task = std::move(m_queue.front());
+          m_queue.pop();
+        }
+        try { task(); } catch (const std::exception& e) { std::cerr << "[ThreadPool] task exception: " << e.what() << "\n"; }
+      }
+    }
+    
+    std::vector<std::thread> m_workers;
+    std::queue<std::function<void()>> m_queue;
+    std::mutex m_mutex;
+    std::condition_variable m_cv;
+    bool m_done;
+  };
+  // Cache key representing symbol+interval+range
+  struct CacheKey
+  {
+    std::string symbol;
+    std::string interval;
+    std::string range;
+    
+    bool operator==(const CacheKey& o) const noexcept
+    {
+      return symbol == o.symbol && interval == o.interval && range == o.range;
+    }
+  };
+
+  struct CacheKeyHash
+  {
+    size_t operator()(const CacheKey& k) const noexcept
+    {
+      size_t h1 = std::hash<std::string>{}(k.symbol);
+      size_t h2 = std::hash<std::string>{}(k.interval);
+      size_t h3 = std::hash<std::string>{}(k.range);
+      return h1 ^ (h2 * 16777619u) ^ (h3 << 1);
+    }
+  };
+
   class StockManager
   {
   public:
+    inline static std::unordered_map<CacheKey, StockData, CacheKeyHash> s_stockDataCache;
+    inline static std::unordered_map<std::string,StockData> s_activeCache;
+    inline static std::vector<std::string> s_symbols;
+    inline static std::string s_selectedStockSymbol;
+    inline static std::string s_currentInterval = "1d";
+    inline static std::string s_currentRange = "1y";
   };
 } // namespace KanVest
