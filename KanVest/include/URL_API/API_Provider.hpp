e6facd068fb55d0acc36038ace0bdd818236eff7
Ashish <aashishkan1009@gmail.com>
2026-01-08 19:59:38 +0530

    URL API provider

---
+++ KanVest/include/URL_API/API_Provider.hpp
@@ -0,0 +1,75 @@
+//
+//  API_Provider.hpp
+//  KanVest
+//
+//  Created by Ashish . on 08/01/26.
+//
+
+#pragma once
+
+namespace KanVest
+{
+  enum class Range
+  {
+    _1D, _5D, _1MO, _6MO, _YTD, _1Y, _5Y, _MAX
+  };
+  
+  enum class Interval
+  {
+    _1M, _2M, _5M, _15M, _30M, _60M, _90M, _1H, _1D, _5D, _1WK, _1MO, _3MO
+  };
+  
+  enum class StockAPIProvider
+  {
+    Yahoo,
+  };
+  
+  struct APIKeys
+  {
+    std::string price = "";
+    std::string prevClose = "";
+    std::string changePercent = "";
+    std::string volume = "";
+    std::string shortName = "";
+    std::string longName = "";
+    std::string instrumentType = "";
+    std::string timezone = "";
+    std::string range = "";
+    std::string dataGranularity = "";
+    std::string currency = "";
+    std::string exchangeName = "";
+    std::string fiftyTwoHigh = "";
+    std::string fiftyTwoLow = "";
+    std::string dayHigh = "";
+    std::string dayLow = "";
+  };
+  
+  /// This class stores the server URL from where data need to be extracted
+  class API_Provider
+  {
+  private:
+    /// This function initializes the API Provide for URL
+    /// - Parameter apiProvider: API provider type
+    static void Initialize(StockAPIProvider apiProvider);
+    
+    /// This function returns the API Keys
+    static APIKeys GetAPIKeys();
+    
+    /// This function returns the URL based on API provider
+    static std::string GetURL();
+    
+    /// This function retruns double data from patterns
+    /// - Parameter key: key string
+    static std::string GetValueParserPattern(const std::string& key);
+    /// This function retruns string data from patterns
+    /// - Parameter key: key string
+    static std::string GetStringParserPattern(const std::string& key);
+    /// This function retruns array data from patterns
+    /// - Parameter key: key string
+    static std::string GetArrayParserPattern(const std::string& key);
+    
+  private:
+    inline static StockAPIProvider s_stockAPIProvider;
+    inline static APIKeys s_apiKeys;
+  };
+} // namespace KanVest
---
+++ KanVest/src/URL_API/API_Provider.cpp
@@ -0,0 +1,75 @@
+//
+//  API_Provider.cpp
+//  KanVest
+//
+//  Created by Ashish . on 08/01/26.
+//
+
+#include "API_Provider.hpp"
+
+namespace KanVest
+{
+  void API_Provider::Initialize(StockAPIProvider apiProvider)
+  {
+    s_stockAPIProvider = apiProvider;
+    
+    switch (s_stockAPIProvider)
+    {
+      case StockAPIProvider::Yahoo:
+        s_apiKeys.price            = "regularMarketPrice";
+        s_apiKeys.prevClose        = "chartPreviousClose";
+        s_apiKeys.changePercent    = "regularMarketChangePercent";
+        s_apiKeys.volume           = "regularMarketVolume";
+        
+        s_apiKeys.shortName        = "shortName";
+        s_apiKeys.longName         = "longName";
+        s_apiKeys.currency         = "currency";
+        s_apiKeys.exchangeName     = "exchangeName";
+        
+        s_apiKeys.instrumentType   = "instrumentType";
+        s_apiKeys.dataGranularity  = "dataGranularity";
+        s_apiKeys.range            = "range";
+        s_apiKeys.timezone         = "timezone";
+        
+        s_apiKeys.fiftyTwoHigh     = "fiftyTwoWeekHigh";
+        s_apiKeys.fiftyTwoLow      = "fiftyTwoWeekLow";
+        s_apiKeys.dayHigh          = "regularMarketDayHigh";
+        s_apiKeys.dayLow           = "regularMarketDayLow";
+        
+        break;
+        
+      default:
+        IK_ASSERT(false, "Invalid API")
+    }
+  }
+  
+  APIKeys API_Provider::GetAPIKeys()
+  {
+    return s_apiKeys;
+  }
+  
+  std::string API_Provider::GetURL()
+  {
+    switch (s_stockAPIProvider)
+    {
+      case StockAPIProvider::Yahoo : return "https://query1.finance.yahoo.com/v8/finance/chart/";
+      default:
+        IK_ASSERT(false, "Invalid API")
+    }
+    return "";
+  }
+  
+  std::string API_Provider::GetValueParserPattern(const std::string& key)
+  {
+    return "\"" + key + "\"\\s*:\\s*([-+]?[0-9]*\\.?[0-9]+)";
+  }
+  
+  std::string API_Provider::GetStringParserPattern(const std::string& key)
+  {
+    return "\"" + key + "\"\\s*:\\s*\"([^\"]+)\"";
+  }
+  std::string API_Provider::GetArrayParserPattern(const std::string& key)
+  {
+    return "\"" + key + "\"\\s*:\\s*\\[([^\\]]+)\\]";
+  }
+} // namespace KanVest
