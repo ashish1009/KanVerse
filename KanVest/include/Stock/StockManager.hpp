4af1e14b30190e4d465ecab99294626befe859ba
Ashish <aashishkan1009@gmail.com>
2026-01-08 20:20:48 +0530

    Starting stock manager

--- KanVest/include/Stock/StockManager.hpp
+++ KanVest/include/Stock/StockManager.hpp
@@ -13,6 +13,17 @@
 
 namespace KanVest
 {
+  /// This structure stores the stock symbol to request data from URL
+  struct StockRequest
+  {
+    std::string symbol;
+    Range range;
+    Interval interval;
+    
+    StockData cachedData;
+    std::chrono::steady_clock::time_point lastUpdated;
+  };
+
   /// This class managers stocks data
   class StockManager
   {
@@ -21,5 +32,23 @@ namespace KanVest
     static void Initialize(int milliseconds = 10);
     /// This function shuts down the stock manager data
     static void Shutdown();
+    
+    /// This function adds the request for stock
+    /// - Parameters:
+    ///   - symbol: stock symbpl
+    ///   - range: range of stock fetch
+    ///   - interval: interval of stock fetch
+    static void AddStockDataRequest(const std::string& symbol, Range range, Interval interval);
+
+  private:
+    /// This is worker loop
+    static void WorkerLoop();
+
+    inline static std::unordered_map<std::string, StockRequest> s_stockDataRequests;
+
+    inline static std::mutex s_mutex;
+    inline static std::atomic<bool> s_running = false;
+    inline static std::thread s_worker;
+    inline static std::atomic<int> s_updateDelayMs = 10;
   };
 } // namespace KanVest
--- KanVest/src/Stock/StockManager.cpp
+++ KanVest/src/Stock/StockManager.cpp
@@ -11,11 +11,29 @@ namespace KanVest
 {
   void StockManager::Initialize(int milliseconds)
   {
-
+    s_running = true;
+    s_worker = std::thread(WorkerLoop);
+    
+    s_updateDelayMs = milliseconds;
   }
   
   void StockManager::Shutdown()
   {
+    s_running = false;
+    if (s_worker.joinable())
+    {
+      s_worker.join();
+    }
+  }
+  
+  void StockManager::AddStockDataRequest(const std::string& symbol, Range range, Interval interval)
+  {
+    std::scoped_lock lock(s_mutex);
+    s_stockDataRequests[symbol] = { symbol, range, interval, StockData(symbol), std::chrono::steady_clock::now() };
+  }
 
+  void StockManager::WorkerLoop()
+  {
+    
   }
 } // namespace KanVest
